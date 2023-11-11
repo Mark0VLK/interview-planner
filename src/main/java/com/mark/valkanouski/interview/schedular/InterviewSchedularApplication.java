b044579de857cce7c35f19ddcd8921af7360f9a5
Марк <markvl836@gmail.com>
2023-11-11 15:05:48 +0300

    add pom.xml

---
+++ src/main/java/com/mark/valkanouski/interview/schedular/InterviewSchedularApplication.java
@@ -0,0 +1,13 @@
+package com.mark.valkanouski.interview.schedular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewSchedularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewSchedularApplication.class, args);
+	}
+
+}
